996bc0fd403d96084bf7d366b78cddb55731fb15
Ahmed Shariff <shariff.mfa@outlook.com>
2023-08-07 23:52:39 -0700

    UI for gaze looking & fix gaze tracking

---
+++ Assets/Scripts/Editor/GazeFocusTrackerEditor.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UXF;
+using ubc.ok.ovilab.ViconUnityStream;
+
+namespace ubc.ok.ovilab.hpuiInSituComparison.study1
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(GazeFocusTracker), true)]
+    public class GazeFocusTrackerEditor: UnityEditor.Editor
+    {
+        GazeFocusTracker t;
+        Transform targetTransform;
+
+        void OnEnable()
+        {
+            t = target as GazeFocusTracker;
+        }
+        
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            targetTransform = (Transform)EditorGUILayout.ObjectField("Target Transform", targetTransform, typeof(Transform), true);
+            if (GUILayout.Button("Look at transform") && targetTransform != null)
+            {
+                t.transform.rotation = Quaternion.LookRotation(targetTransform.position - t.transform.position);
+            }
+        }
+    }
+}
