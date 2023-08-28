15aa225a937a33e792d8a6196798635c45826285
Shariff Faleel <shariff.mfa@outlook.com>
2023-08-28 11:07:50 -0700

    Update configs & add executable

---
+++ External/run-study.bat
@@ -0,0 +1,6 @@
+start "" /b "..\Builds\HPUI-in-situ-comparison.exe"
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mdate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mtime=%%a%%b)
+xcopy C:\Users\amshamoh\AppData\LocalLow\ubc_ok_ovilab\HPUI-in-situ-comparison C:\Users\amshamoh\AppData\LocalLow\ubc_ok_ovilab\HPUI-in-situ-comparison_back_%mdate%_%mtime% /e /q /i
+start poetry run experiment-server run .\Assets\hpui-in-situ-comparison-study1.toml
+start "" "http://127.0.0.1:5000"
