14f381ad699b830b73abf891f5cc7629d8a598e4
Ahmed Shariff <shariff.mfa@outlook.com>
2024-01-16 21:41:59 -0800

    Fix for tap color behavior not working correctly

--- Assets/Scripts/study2/InteractableColorBehaviour.cs
+++ Assets/Scripts/study2/InteractableColorBehaviour.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Linq;
 using ubco.ovilab.HPUI.Interaction;
 using UnityEngine;
@@ -10,20 +11,39 @@ namespace ubco.ovilab.hpuiInSituComparison.study2
         public HPUIBaseInteractable interactable;
         public Renderer targetRenderer;
 
+        private bool awaitingDelayedReset = false;
+        private string stateAfterDelayedReset = null;
+
         public void OnEnable()
         {
             if (interactable != null)
             {
                 interactable.hoverEntered.AddListener(_ => SetColor("hovered"));
                 interactable.selectEntered.AddListener(_ => SetColor("selected"));
-                interactable.TapEvent.AddListener(_ => SetColor("tap"));
+                interactable.TapEvent.AddListener(_ => StartCoroutine(SetWithDelayedReset("tap")));
                 interactable.selectExited.AddListener(_ => SetColor("hovered"));
                 interactable.hoverExited.AddListener(_ => SetColor());
             }
         }
 
-        public void SetColor(string state=null)
+        private IEnumerator SetWithDelayedReset(string state)
         {
+            SetColor(state);
+            awaitingDelayedReset = true;
+            yield return new WaitForSeconds(0.1f);
+            awaitingDelayedReset = false;
+            SetColor(stateAfterDelayedReset);
+            stateAfterDelayedReset = null;
+        }
+
+        private void SetColor(string state=null)
+        {
+            if (awaitingDelayedReset)
+            {
+                stateAfterDelayedReset = state;
+                return;
+            }
+
             Color c;
             if (string.IsNullOrEmpty(state))
             {
